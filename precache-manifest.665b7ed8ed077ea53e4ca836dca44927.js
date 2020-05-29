d1b7c5e4b4258c7dee37be1ed59baf01ed0ffbf8
Daniel <virusxd521@gmail.com>
2020-05-29 17:36:44 +0300

    Updates

---
+++ precache-manifest.665b7ed8ed077ea53e4ca836dca44927.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451b91596c2557cdc325909eca22cd5d",
+    "url": "/robotnew/index.html"
+  },
+  {
+    "revision": "3b3761d97755b5fa4ba3",
+    "url": "/robotnew/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3ce16ffc384c60bc1f5f",
+    "url": "/robotnew/static/css/main.118f87fe.chunk.css"
+  },
+  {
+    "revision": "3b3761d97755b5fa4ba3",
+    "url": "/robotnew/static/js/2.b7db9bba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotnew/static/js/2.b7db9bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce16ffc384c60bc1f5f",
+    "url": "/robotnew/static/js/main.e505b07c.chunk.js"
+  },
+  {
+    "revision": "3af983fb2144f0e23902",
+    "url": "/robotnew/static/js/runtime-main.e347b85a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotnew/static/media/SEGA.12a3db8b.woff"
+  }
+]);
